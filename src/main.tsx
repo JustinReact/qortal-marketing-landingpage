25dde1817b8026d21fee32ba4fb7556f76557e11
Phillip Lang Martinez <philliplangmartinez@gmail.com>
2023-05-23 00:24:16 +0300

    qdn changes

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,16 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
 
+interface CustomWindow extends Window {
+  _qdnBase: any; // Replace 'any' with the appropriate type if you know it
+}
+
+const customWindow = window as unknown as CustomWindow;
+
+// Now you can access the _qdnTheme property without TypeScript errors
+const baseUrl = customWindow?._qdnBase || "";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter basename={baseUrl}>
+    <App />
+  </BrowserRouter>
 );
