784493ea838602b1675b3a1d142e32d5724c402c
Justin Ferrari <justinwesleyferrari@gmail.com>
2023-11-12 10:12:06 -0500

    Removed unneeded code and uncommented baseUrl

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import ReactGA from "react-ga4";
 import { Provider } from "react-redux";
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { lightTheme, darkTheme } from "./styles/theme";
-import { NotFound } from "./pages/NotFound/NotFound";
 import Api from "./pages/Docs/Api/Api";
 import { store } from "./state/store";
 import { MainLayoutRoute } from "./components/Layout/Main/MainLayout";
-
-// Initialize Google Analytics
-ReactGA.initialize("G-E1BB62FVTN");
 
 function App() {
   const [theme, setTheme] = useState<string>("dark");
@@ -30,7 +25,6 @@ function App() {
               path="/"
               element={<Api setTheme={(val: string) => setTheme(val)} />}
             />
-            <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
       </ThemeProvider>
