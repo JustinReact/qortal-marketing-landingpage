6ed6d6ad3419f3a998611431f8ea103813e71208
PhilReact <philliplangmartinez@gmail.com>
2024-09-17 22:01:59 +0300

    test

--- src/components/Api/ApiSectionsExtension/Introduction/Introduction.tsx
+++ src/components/Api/ApiSectionsExtension/Introduction/Introduction.tsx
@@ -10,8 +10,7 @@ interface SectionProps {
   setSelectedSection: (sectionId: string) => void;
   id: string;
 }
-// test commit
-// Test 2
+
 export const Introduction: FC<SectionProps> = ({
   title,
   setSelectedSection,
