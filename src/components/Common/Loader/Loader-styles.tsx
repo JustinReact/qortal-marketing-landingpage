87713051b5ced9198cb239acab6782b97723d824
Justin Ferrari <justinwesleyferrari@gmail.com>
2024-10-11 13:41:30 +0300

    Added individual blog page & loading page

---
+++ src/components/Common/Loader/Loader-styles.tsx
@@ -0,0 +1,32 @@
+import { Box, keyframes, styled } from "@mui/system";
+
+interface LoadingProps {
+  delay: number;
+}
+
+// Animation keyframes
+const fadeIn = keyframes`
+  0%, 100% {
+    opacity: 0;
+    transform: scale(0.5);
+  }
+  20%, 80% {
+    opacity: 1;
+    transform: scale(1);
+  }
+`;
+
+export const LoadingContainer = styled(Box)({
+  display: "flex",
+  gap: "8px",
+  flexDirection: "row",
+  alignItems: "center",
+});
+
+export const LoadingQortalLogo = styled(Box)<LoadingProps>(
+  ({ delay }) => ({
+    opacity: 0, // Start invisible
+    animation: `${fadeIn} 4.5s ease infinite`,
+    animationDelay: `${delay}s`,
+  })
+);
