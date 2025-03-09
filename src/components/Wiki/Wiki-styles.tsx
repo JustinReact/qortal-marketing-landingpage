312640286a0c6baabbcffd65f96d305d98eabbb8
Justin Ferrari <justinwesleyferrari@gmail.com>
2025-03-09 13:39:01 +0200

    Continued wiki mobile logic

--- src/components/Wiki/Wiki-styles.tsx
+++ src/components/Wiki/Wiki-styles.tsx
@@ -1,27 +1,61 @@
 import { styled } from "@mui/system";
 import { Box } from "@mui/material";
+import { oxygen } from "../../app/fonts";
+import { ChevronLeftSVG } from "../Common/Icons/ChevronLeftSVG";
 
 type MainContainerProps = {
   showInFullScreenMobile: boolean;
   isMobile: boolean;
 }
 
-export const WikiContainer = styled(Box)(({ theme }) => ({
+export const WikiContainer = styled(Box, {
+  shouldForwardProp: (prop) => prop !== "showInFullScreenMobile" && prop !== "isMobile",
+})<MainContainerProps>(({ isMobile, showInFullScreenMobile, theme }) => ({
   display: "grid",
-  gridTemplateColumns: "1fr 3fr",
+  gridTemplateColumns: isMobile && showInFullScreenMobile ? "1fr" : "1fr 3fr",
   gap: "50px",
   padding: "50px",
   width: "100%",
 }));
 
 export const MainContainer = styled(Box, {
-  shouldForwardProp: (prop) => prop !== "isExpanded"
+  shouldForwardProp: (prop) => prop !== "showInFullScreenMobile" && prop !== "isMobile",
 })<MainContainerProps>(({ isMobile, showInFullScreenMobile, theme }) => ({
   display: !showInFullScreenMobile && isMobile ? "none" :  "flex",
   flexDirection: "column",
+  alignItems: showInFullScreenMobile ? "center" : "flex-start",
   padding: "15px",
-  position: showInFullScreenMobile ? "fixed" : "relative",
-  left: showInFullScreenMobile ? "0" : "unset",
-  width: showInFullScreenMobile ? "100%" : "auto",
-  height: showInFullScreenMobile ? "100vh" : "auto",
 }));
+
+export const MobileSectionListContainer = styled(Box)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "flex-start",
+  gap: "34px",
+  marginBottom: "80px",
+}));
+
+export const MobileSectionList = styled(Box)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "flex-start",
+  gap: "38px",
+  width: "100%",
+}));
+
+export const SectionTitleMobile = styled(Box)(({ theme }) => ({
+  textTransform: "uppercase",
+  fontFamily: oxygen.style.fontFamily,
+  fontSize: "20px",
+  fontWeight: "bold",
+  lineHeight: "27px",
+  letterSpacing: 0,
+  userSelect: "none",
+  cursor: "pointer",
+  color: theme.palette.text.primary,
+}));
+
+export const ChevronIcon = styled(ChevronLeftSVG)({
+  marginTop: "5px",
+  cursor: "pointer",
+})
--- src/components/Wiki/Wiki.tsx
+++ src/components/Wiki/Wiki.tsx
@@ -1,21 +1,30 @@
 "use client";
 import React, { FC, useEffect, useState, useTransition } from "react";
-import { MainContainer, WikiContainer } from "./Wiki-styles";
+import {
+  ChevronIcon,
+  MainContainer,
+  MobileSectionList,
+  MobileSectionListContainer,
+  SectionTitleMobile,
+  WikiContainer
+} from "./Wiki-styles";
 import { Sidebar } from "./Sidebar/Sidebar";
 import { WikiProps } from "../../app/wiki/types";
 import { usePathname, useRouter } from "next/navigation";
 import LoadingSpinner from "../../app/loading";
-import { useMediaQuery } from "@mui/material";
+import { useMediaQuery, useTheme } from "@mui/material";
 
 export const Wiki: FC<WikiProps> = ({ title, children, sections }) => {
   const router = useRouter();
   const isMobile = useMediaQuery("(max-width: 1130px)");
   const pathname = usePathname();
+  const theme = useTheme();
 
   const [isPending, startTransition] = useTransition();
   const [loadingPage, setLoadingPage] = useState<string | null>(null);
   const [isExpandedMobile, setExpandedMobile] = useState<boolean>(false);
 
+  console.log(pathname, "pathname");
   const handleNavigation = (url: string) => {
     setLoadingPage(url);
     startTransition(() => {
@@ -30,8 +39,13 @@ export const Wiki: FC<WikiProps> = ({ title, children, sections }) => {
     }
   }, [pathname, isMobile]);
 
+  console.log(sections, "sections");
+
   return (
-    <WikiContainer>
+    <WikiContainer
+      isMobile={isMobile}
+      showInFullScreenMobile={isMobile && isExpandedMobile}
+    >
       <Sidebar
         showInFullScreenMobile={isMobile && !isExpandedMobile}
         setExpandedMobile={setExpandedMobile}
@@ -40,7 +54,50 @@ export const Wiki: FC<WikiProps> = ({ title, children, sections }) => {
         }}
         sections={sections}
       />
-      <MainContainer isMobile={isMobile} showInFullScreenMobile={isMobile && isExpandedMobile}>
+      <MainContainer
+        isMobile={isMobile}
+        showInFullScreenMobile={isMobile && isExpandedMobile}
+      >
+        {/* Show this only on mobile, and only if they've clicked on a Section Title */}
+        {isMobile && isExpandedMobile && (
+          <MobileSectionListContainer>
+            <ChevronIcon
+              color={theme.palette.text.primary}
+              height="20px"
+              width="20px"
+              onClickFunc={() => {
+                setExpandedMobile(false);
+                handleNavigation("/wiki");
+              }}
+            />
+            <>
+              {Object.entries(sections).map(([_, pages]) => (
+                <>
+                  {pages
+                    .filter((page) => page.url === pathname)
+                    .map((page) => {
+                      return (
+                        <MobileSectionList key={page.url}>
+                          <>
+                            {page.headings
+                              .filter((heading) => heading.depth === 2)
+                              .map((heading) => (
+                                <SectionTitleMobile
+                                  key={heading.id}
+                                  // onClick={() => handleNavigation(`${page.url}#${heading.id}`)}
+                                >
+                                  {heading.title}
+                                </SectionTitleMobile>
+                              ))}
+                          </>
+                        </MobileSectionList>
+                      );
+                    })}
+                </>
+              ))}
+            </>
+          </MobileSectionListContainer>
+        )}
         {loadingPage ? <LoadingSpinner /> : children}
       </MainContainer>
     </WikiContainer>
