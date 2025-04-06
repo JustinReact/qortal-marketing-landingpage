b0516a5209ce20fe7b3fca1e5864413001b69b17
Justin Ferrari <justinwesleyferrari@gmail.com>
2025-04-06 18:32:14 +0300

    Update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -67,6 +67,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 1,
     },
     {
+      url: 'https://qortal.dev/wiki',
+      lastModified: new Date(),
+      changeFrequency: 'monthly' as const,
+      priority: 0.6
+    },
+    {
       url: 'https://qortal.dev/support',
       lastModified: new Date(),
       changeFrequency: 'yearly' as const,
