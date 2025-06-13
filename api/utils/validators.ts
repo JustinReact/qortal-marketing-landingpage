a125d5ef59a8e4a08c643f667254606e8aaacfdf
Justin Ferrari <justinwesleyferrari@gmail.com>
2025-06-13 09:13:04 +0300

    Remove console.log

--- api/utils/validators.ts
+++ api/utils/validators.ts
@@ -2,7 +2,6 @@ import validator from "validator";
 
 // Utility function to check if a string is a valid email
 const isEmail = (email: string) => {
-    console.log("Validating email:", email);
   return validator.isEmail(email);
 };
 
